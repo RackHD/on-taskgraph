d2ad31bee7a86d73f7d7aa6e0e185290a6558ce3
Dave Quick <dave@quicks.me>
2015-01-12 00:38:28 -0800

    commit WIP

--- prototype/sample.js
+++ prototype/sample.js
@@ -1,28 +1,42 @@
 'use strict';
 var Rx = require('rx');
-var events = require('events');
 var util = require('util');
+var events = require('events');
 var eventEmitter = new events.EventEmitter();
+var uuid = require('node-uuid');
 
 
 function Scheduler(){
     this.toRun = [];
-    this.Running= [];
+    this.Running= {};
     this.runComplete = [];
     this.maxConcurrent = 3;
     this.currentlyRunning = 0;
-    this.on('scheduled', function(){this.checkWork()});
-    this.on('completed', function(){this.checkWork()});
+    this.on('scheduled', this.evaluateWork.bind(this));
+    this.on('completed', this.evaluateWork.bind(this));
 }
 util.inherits(Scheduler, events.EventEmitter);
 
+Scheduler.prototype.wrapData = function(taskData) {
+    return {
+        taskData:taskData,
+        id: uuid.v4(),
+        stats: {
+            created: new Date(),
+            started: null,
+            completed: null
+0        }
+    };
+};
+
 Scheduler.prototype.schedule = function(data){
-    console.log('SCHEDULING: received new work: iteration: '+data.iteration + ', lastdelay:'+data.lastDelay);
-    this.toRun.push(data);
+    var workItem = this.wrapData(data);
+    console.log('SCHEDULING: '+ workItem.id + ', iteration: '+data.iteration + ', lastdelay:'+data.lastDelay);
+    this.toRun.push(workItem);
     this.emit('scheduled');
 };
 
-Scheduler.prototype.checkWork= function() {
+Scheduler.prototype.evaluateWork= function() {
     console.log('checkwork started')
     if(this.toRun.length == 0){
         console.log('no work ito be run');
@@ -33,16 +47,22 @@ Scheduler.prototype.checkWork= function() {
         return;
     }
     this.currentlyRunning += 1;
-    var next = this.toRun.shift();
-    console.log('CHECKWORKRUNNING TASK: '+ next.iteration)
-    this.Running.push(next);
+    var nextWorkItem = this.toRun.shift();
+    nextWorkItem.stats.started = new Date();
+    console.log('CHECKWORKRUNNING TASK: '+ nextWorkItem.id);
+    this.Running[nextWorkItem.id] = nextWorkItem;
     var self = this;
-    doArbitraryWork(function(){self.done.apply(self,arguments)},next);
+    doArbitraryWork(function(){
+        self.done(nextWorkItem,arguments[1]);
+    },nextWorkItem.taskData);
 };
 
-Scheduler.prototype.done = function(data) {
-    console.log('RUNCOMPLETE: ' + data.iteration );
-    this.runComplete.push(data);
+Scheduler.prototype.done = function(completedWorkItem, output) {
+    console.log('RUNCOMPLETE: ' + completedWorkItem.taskData.iteration );
+    delete this.Running[completedWorkItem.id];
+    completedWorkItem.stats.finished = new Date();
+    console.dir(completedWorkItem);
+    this.runComplete.push(completedWorkItem);
     this.currentlyRunning -= 1;
     this.emit('completed');
 };
@@ -52,7 +72,7 @@ function randomInt (low, high) {
 }
 
 function doArbitraryWork(done, data){
-    var delay = randomInt(10000,12000);
+    var delay = randomInt(1000,2000);
 
     setTimeout(function(){
         done({iteration:data.iteration, totalWorktime: delay});
@@ -76,3 +96,4 @@ var subscription = source.subscribe(function(data){scheduler.schedule(data)});
 
 
 arbitraryExampleEventGenerator(0, 0);
+*setTimeout(function(){console.log('test')}, 60000);
