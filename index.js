31a4fea11d4b7e1f047f3e1316054e655dc824d7
Ben Broderick Phillips <ben@benbp.net>
2015-01-15 19:23:55 -0800

    Snapshot. Graph loader, updates

--- index.js
+++ index.js
@@ -9,18 +9,27 @@ var di = require('di'),
     injector = new di.Injector(
         _.flatten([
             core.injectables,
+            //require('renasar-tasks'),
+            require('../renasar-tasks/lib/tasks/abstract-tasks/task'),
             require('./lib/app'),
-            require('./lib/task-graph-runner')
+            require('./lib/task-graph'),
+            require('./lib/graphs/graph-loader'),
+            require('./lib/task-graph-runner'),
+            require('./lib/scheduler'),
+            require('./lib/registry')
         ])
     ),
-    taskGraphRunner = injector.get('TaskGraph.Runner'),
-    logger = injector.get('Logger').initialize('TaskGraph.Runner');
+    taskGraphService = injector.get('TaskGraph'),
+    logger = injector.get('Logger').initialize('TaskGraph');
 
-taskGraphRunner.start()
+
+debugger;
+taskGraphService.start()
     .then(function () {
         logger.info('Task Graph Runner Started.');
     })
     .catch(function(error) {
+        console.error(error);
         logger.error('Task Graph Runner Startup Error.', { error: error });
 
         process.nextTick(function() {
@@ -29,7 +38,7 @@ taskGraphRunner.start()
     });
 
 process.on('SIGINT', function() {
-    taskGraphRunner.stop()
+    taskGraphService.stop()
         .catch(function(error) {
             logger.error('Task Graph Runner Shutdown Error.', { error: error });
         })
