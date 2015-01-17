725cacbd652b2d3526d89dce7a8fdfab4d4f1f36
Ben Broderick Phillips <ben@benbp.net>
2015-01-17 13:51:50 -0800

    Fill in _findReadyTasks()

--- spec/lib/task-graph-spec.js
+++ spec/lib/task-graph-spec.js
@@ -87,16 +87,55 @@ describe("Task Graph", function () {
         var graphFactory = this.registry.fetchGraph('Graph.test');
         var graph = graphFactory.create();
 
-        graph.populateTaskDependencies();
+        graph._populateTaskDependencies();
 
         expect(graph.tasks).to.be.ok;
-        expect(graph.tasks[0]).to.be.ok;
-        expect(graph.tasks[0].instanceId).to.be.a.uuid;
-        expect(graph.tasks[1]).to.be.ok;
-        expect(graph.tasks[1].instanceId).to.be.ok;
-        expect(graph.tasks[0].waitingOn).to.be.empty;
-        expect(graph.tasks[1].waitingOn).to.have.property(
-            graph.tasks[0].instanceId
+        expect(_.keys(graph.tasks)).to.have.length(2);
+
+        var taskWithDependencies,
+            taskWithNoDependencies;
+
+        _.forEach(graph.tasks, function(v) {
+            if (_.isEmpty(v.waitingOn)) {
+                taskWithNoDependencies = v;
+            } else {
+                taskWithDependencies = v;
+            }
+        });
+        expect(taskWithDependencies).to.be.ok;
+        expect(taskWithNoDependencies).to.be.ok;
+
+        expect(taskWithDependencies.instanceId).to.be.a.uuid;
+        expect(taskWithNoDependencies.instanceId).to.be.a.uuid;
+
+        expect(taskWithNoDependencies.waitingOn).to.be.empty;
+        expect(taskWithDependencies.waitingOn).to.have.property(
+            taskWithNoDependencies.instanceId
         ).that.equals('finished');
+    });
+
+    it("should find ready tasks", function() {
+        var TaskGraph = this.injector.get('TaskGraph.TaskGraph');
+        var Task = this.injector.get('Task.Task');
+        var loader = this.injector.get('TaskGraph.DataLoader');
+        loader.loadTasks([baseTask, testTask], Task.createRegistryObject);
+        loader.loadGraphs([graphDefinition], TaskGraph.createRegistryObject);
+        var graphFactory = this.registry.fetchGraph('Graph.test');
+        var graph = graphFactory.create();
+        graph._populateTaskDependencies();
+
+        graph._findReadyTasks();
+        expect(graph.ready).to.be.empty;
+
+        var taskWithDependencies;
+        _.forEach(graph.tasks, function(v) {
+            if (_.isEmpty(v.waitingOn)) {
+                taskWithDependencies = v;
+            }
+        });
+        taskWithDependencies.outcome = 'finished';
+
+        graph._findReadyTasks();
+        expect(graph.ready).to.not.be.empty;
     });
 });
