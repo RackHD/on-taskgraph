028ffcde067b5681080396acbc5a0aed5037e838
Jason Gardino <jason.gardino@renasar.com>
2015-02-02 12:57:39 -0800

    adding standalone Generate SKU graph

---
+++ spec/lib/graphs/generate-sku-graph.js
@@ -0,0 +1,17 @@
+// Copyright 2015, Renasar Technologies Inc.
+/* jshint node:true */
+
+'use strict';
+
+describe(__filename, function () {
+    var base = require('./base-graph-spec');
+
+    base.before(function (context) {
+        context.taskdefinition = helper.require('/lib/graphs/generate-sku-graph.js');
+    });
+
+    describe('graph', function () {
+        base.examples();
+    });
+
+});
