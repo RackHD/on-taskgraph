48fec35cea252362be3f36a694238eeb68f29249
Ben Broderick Phillips <ben.broderickphillips@emc.com>
2015-05-08 12:25:03 -0700

    Add graph tests for create-*-obm-settings-graph

---
+++ spec/lib/graphs/create-vbox-obm-settings-graph-spec.js
@@ -0,0 +1,17 @@
+// Copyright 2015, Renasar Technologies Inc.
+/* jshint node:true */
+
+'use strict';
+
+describe(require('path').basename(__filename), function () {
+    var base = require('./base-graph-spec');
+
+    base.before(function (context) {
+        context.taskdefinition = helper.require('/lib/graphs/create-vbox-obm-settings-graph');
+    });
+
+    describe('graph', function () {
+        base.examples();
+    });
+
+});
