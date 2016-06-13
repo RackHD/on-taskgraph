1c509560325c5a5dea5e3718cdddf09aa64a4c24
Andre keedy <andre.keedy@emc.com>
2016-06-13 08:53:10 -0400

    Add Unit tests

---
+++ spec/lib/graphs/bootstrap-decommission-node-graph-spec.js
@@ -0,0 +1,18 @@
+// Copyright 2016, EMC, Inc.
+/* jshint node:true */
+
+'use strict';
+
+describe(require('path').basename(__filename), function () {
+    var base = require('./base-graph-spec');
+
+    base.before(function (context) {
+        context.taskdefinition = 
+            helper.require('/lib/graphs/bootstrap-decommission-node-graph.js');
+    });
+
+    describe('graph', function () {
+        base.examples();
+    });
+
+});
