925f83d48a7435e2856fb59e4d8bf0867f823c67
Torrent Glenn <torrent.glenn@emc.com>
2017-03-21 17:19:19 -0700

    added WIP chef server install graph

---
+++ spec/lib/graphs/install-chef-server-graph-spec.js
@@ -0,0 +1,19 @@
+//Copyright © 2017 Dell Inc. or its subsidiaries. All Rights Reserved.
+/* jshint node:true */
+
+'use strict';
+
+describe(require('path').basename(__filename), function () {
+    var base = require('./base-graph-spec');
+
+    base.before(function (context) {
+        context.taskdefinition = helper.require(
+            '/lib/graphs/examples/install-chef-server-graph.js'
+        );
+    });
+
+    describe('graph', function () {
+        base.examples();
+    });
+
+});
