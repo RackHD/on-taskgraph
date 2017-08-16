f6c055c3a64d4ee98c864fdfc76f800ee5157555
Anitha Srinivasan <Anitha.Srinivasan@dell.com>
2017-08-16 14:25:57 -0400

    Create new workflow for Quanta megaraid catalog
    Signed-off-by: Kranti Uppala <kranti.uppala@emc.com>

---
+++ spec/lib/graphs/quanta-storcli-megaraid-catalog-graph-spec.js
@@ -0,0 +1,17 @@
+// Copyright 2017, Dell EMC, Inc.
+/* jshint node:true */
+
+'use strict';
+
+describe(require('path').basename(__filename), function () {
+    var base = require('./base-graph-spec');
+
+    base.before(function (context) {
+        context.taskdefinition = helper.require('/lib/graphs/quanta-storcli-megaraid-catalog-graph.js');
+    });
+
+    describe('graph', function () {
+        base.examples();
+    });
+
+});
