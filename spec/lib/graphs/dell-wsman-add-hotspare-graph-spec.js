f3939ef0ecce4cfb45f415c5dcd2f5e1e05164b8
AlaricChan <cs_dlut@163.com>
2017-12-21 03:53:18 +0000

    Create graph to add hotspare via WSMAN

---
+++ spec/lib/graphs/dell-wsman-add-hotspare-graph-spec.js
@@ -0,0 +1,15 @@
+// Copyright © 2017 Dell Inc. or its subsidiaries. All Rights Reserved.
+
+'use strict';
+
+describe(require('path').basename(__filename), function () {
+    var base = require('./base-graph-spec');
+
+    base.before(function (context) {
+        context.taskdefinition = helper.require('/lib/graphs/dell-wsman-add-hotspare-graph.js');
+    });
+
+    describe('graph', function () {
+        base.examples();
+    });
+});
